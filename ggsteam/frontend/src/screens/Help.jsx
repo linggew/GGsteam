664af9c8deb2e50e87ba76983d8bfdd686d334ac
linggew <linggew2@illinois.edu>
2023-12-02 10:41:09 -0600

    fix format error

--- ggsteam/frontend/src/screens/Help.jsx
+++ ggsteam/frontend/src/screens/Help.jsx
@@ -5,12 +5,12 @@ function Help() {
   return (
     <div>
       <Header />
-      <H1>Help Page</H1>
-        <p>Basic Information:</p>
-        <p>TeamName	FightOne </p>
-        <p>Captain: Jialu Xu (jialu5@illinois.edu)</p>
-        <p>Member1	Tianhao Chen (tc30@illinois.edu)</p>
-        <p>Member2	Shenghao Che (linggew2@illinois.edu)</p>
+      <h1>Help Page</h1>
+      <p>Basic Information:</p>
+      <p>TeamName FightOne </p>
+      <p>Captain: Jialu Xu (jialu5@illinois.edu)</p>
+      <p>Member1 Tianhao Chen (tc30@illinois.edu)</p>
+      <p>Member2 Shenghao Che (linggew2@illinois.edu)</p>
       <Footer />
     </div>
   )
