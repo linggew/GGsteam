5713789fe374cb690c73f7be74bd8ff0817d9b43
Shenghao Cheng <sc71@illinois.edu>
2023-12-02 15:39:09 +0800

    add info for about and help page

--- ggsteam/frontend/src/screens/Help.jsx
+++ ggsteam/frontend/src/screens/Help.jsx
@@ -5,7 +5,12 @@ function Help() {
   return (
     <div>
       <Header />
-      <p>help Page</p>
+      <H1>Help Page</H1>
+        <p>Basic Information:</p>
+        <p>TeamName	FightOne </p>
+        <p>Captain: Jialu Xu (jialu5@illinois.edu)</p>
+        <p>Member1	Tianhao Chen (tc30@illinois.edu)</p>
+        <p>Member2	Shenghao Che (linggew2@illinois.edu)</p>
       <Footer />
     </div>
   )
