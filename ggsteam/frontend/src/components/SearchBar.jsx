2d09b4a39b950c8129210bbea6317c3c480d6378
linggew <linggew2@illinois.edu>
2023-12-02 15:54:39 -0600

    search

--- ggsteam/frontend/src/components/SearchBar.jsx
+++ ggsteam/frontend/src/components/SearchBar.jsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Axios from 'axios'
+import '../App.css'
+import config from '../config'
 
 const SearchBar = ({ onSearch }) => {
+  const [gameList, setGameList] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [filteredGame, setFilteredGame] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await Axios.get(config.apiUrl + '/api/games', {})
+        setGameList(res.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [searchTerm])
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value)
@@ -12,6 +31,11 @@ const SearchBar = ({ onSearch }) => {
     console.log('Search term:', searchTerm)
     setSearchTerm('')
     setIsModalVisible(true)
+    const filtered = gameList.filter((game) =>
+      game.QueryName.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    const limitedFiltered = filtered.slice(0, 10)
+    setFilteredGame(limitedFiltered)
   }
 
   const openModal = () => {
@@ -40,7 +64,25 @@ const SearchBar = ({ onSearch }) => {
             <span className="close" onClick={closeModal}>
               &times;
             </span>
-            <p>Modal content here...</p>
+            <div>
+              <ul className="listcontainer">
+                {filteredGame.map((game, index) => (
+                  <Link to={`/games/${game.query_id}`} className="link">
+                    <li
+                      key={index}
+                      className="listbox"
+                      style={{ cursor: 'pointer' }}>
+                      <img
+                        className="listimg"
+                        src={game.HeaderImage}
+                        alt={`fail to show ${game.QueryName}`}
+                      />
+                      <p>{game.QueryName}</p>
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       )}
