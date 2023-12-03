2ce046c08e2f8b239c5d3620ae216a5a018106c7
linggew <linggew2@illinois.edu>
2023-12-03 15:31:59 -0600

    fix filter css

--- ggsteam/frontend/src/components/GameFilter.jsx
+++ ggsteam/frontend/src/components/GameFilter.jsx
@@ -7,7 +7,6 @@ const GameFilter = ({ onFilter }) => {
   const [isPC, setIsPC] = useState(false)
 
   const handleFilter = () => {
-    // 在这里处理筛选逻辑，并传递筛选条件给父组件
     const filters = {
       category,
       age,
@@ -19,40 +18,37 @@ const GameFilter = ({ onFilter }) => {
 
   return (
     <div>
-      <label>
+      <label style={{ color: 'white', fontWeight: 'bold' }}>
         Category:
         <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Select Category</option>
           <option value="Action">Action</option>
           <option value="Adventure">Adventure</option>
           <option value="Strategy">Strategy</option>
-          {/* 其他选项 */}
         </select>
       </label>
 
-      <label>
+      <label style={{ color: 'white', fontWeight: 'bold' }}>
         Age:
         <select value={age} onChange={(e) => setAge(e.target.value)}>
           <option value="">Select Age</option>
           <option value="E">Everyone</option>
           <option value="E10+">Everyone 10 and older</option>
           <option value="T">Teen</option>
-          {/* 其他选项 */}
         </select>
       </label>
 
-      <label>
+      <label style={{ color: 'white', fontWeight: 'bold' }}>
         Price:
         <select value={price} onChange={(e) => setPrice(e.target.value)}>
           <option value="">Select Price Range</option>
           <option value="20">$0 - $20</option>
           <option value="50">$21 - $50</option>
           <option value="100">$51 - $100</option>
-          {/* 其他选项 */}
         </select>
       </label>
 
-      <label>
+      <label style={{ color: 'white', fontWeight: 'bold' }}>
         PC:
         <select
           value={isPC}
